60ec665f6db269257f750ee7a387744fccc939c5
Estela Alves <123739850+estelalm@users.noreply.github.com>
2024-06-04 14:11:23 -0300

    login não funcionando ainda

--- login/login.js
+++ login/login.js
@@ -1,13 +1,16 @@
 'use strict'
-const btnSignUp = document.getElementById('botao-signup')
-const btnLogin = document.getElementById('botao-login')
+
+import { getLogarUsuario } from "../../api/endpoints.js"
+
+//tela de login e cadastro
+const btnMudarSignUp = document.getElementById('botao-signup')
+const btnMudarLogin = document.getElementById('botao-login')
 
 const mudarParaCriarConta = () =>{
 
     const login = document.getElementById('container-login')
     const signup =document.getElementById('container-signup')
     let width = login.offsetWidth
-    console.log(width)
     login.style.transform = `translateX(-${width}px)`
     signup.style.transform = `translateX(-${width}px)`
 
@@ -16,12 +19,34 @@ const mudarParaLogar = () =>{
 
     const login = document.getElementById('container-login')
     const signup =document.getElementById('container-signup')
-    let width = login.offsetWidth
-    console.log(width)
+    let width = login.offsetWidth / 300
     login.style.transform = `translateX(${width}px)`
     signup.style.transform = `translateX(${width}px)`
 
 }
 
-btnSignUp.addEventListener('click', mudarParaCriarConta)
-btnLogin.addEventListener('click', mudarParaLogar)
+btnMudarSignUp.addEventListener('click', mudarParaCriarConta)
+btnMudarLogin.addEventListener('click', mudarParaLogar)
+
+//login
+
+const btnSignUp = document.getElementById('botao-cadastrar')
+const btnLogin = document.getElementById('botao-logar')
+
+
+const logar = async () =>{
+
+    const email = document.getElementById('input-email').value
+    const senha = document.getElementById('input-senha').value
+    console.log(email,senha)
+    const usuario = await getLogarUsuario(email, senha)
+
+    if(usuario){
+        window.location.assign('../home.html')
+    }else{
+        alert('usuário ou senha incorretos')
+    }
+    
+}
+
+btnLogin.addEventListener('click', logar)

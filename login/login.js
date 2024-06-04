fd471722e24567224e909b95dcb5d4d53c644b0b
Estela Alves <123739850+estelalm@users.noreply.github.com>
2024-06-04 04:44:25 -0300

    login -não terminado

--- login/login.js
+++ login/login.js
@@ -1,2 +1,27 @@
 'use strict'
+const btnSignUp = document.getElementById('botao-signup')
+const btnLogin = document.getElementById('botao-login')
 
+const mudarParaCriarConta = () =>{
+
+    const login = document.getElementById('container-login')
+    const signup =document.getElementById('container-signup')
+    let width = login.offsetWidth
+    console.log(width)
+    login.style.transform = `translateX(-${width}px)`
+    signup.style.transform = `translateX(-${width}px)`
+
+}
+const mudarParaLogar = () =>{
+
+    const login = document.getElementById('container-login')
+    const signup =document.getElementById('container-signup')
+    let width = login.offsetWidth
+    console.log(width)
+    login.style.transform = `translateX(${width}px)`
+    signup.style.transform = `translateX(${width}px)`
+
+}
+
+btnSignUp.addEventListener('click', mudarParaCriarConta)
+btnLogin.addEventListener('click', mudarParaLogar)

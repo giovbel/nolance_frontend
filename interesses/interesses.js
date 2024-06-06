87997f6f8faa97a1b9ccfd2491e06cf36d4dfa71
Giovanna Belo <123781727+giovbel@users.noreply.github.com>
2024-06-06 17:25:06 -0300

    atualizado

---
+++ interesses/interesses.js
@@ -0,0 +1,56 @@
+'use strict'
+
+import { getCategorias } from "../api/endpoints.js";
+
+async function carregarCategorias(){
+    
+const main = document.getElementById('main')
+
+
+const categorias = await getCategorias()
+
+console.log(categorias)
+
+categorias.forEach(interesse => {
+    const divInteresse = document.createElement('div');
+    divInteresse.classList.add('h-40', 'w-40', 'bg-gradient-to-b', 'from-opacity-45', 'from-[#A7CAA8]', 'shadow-lg', 'to-[#dffce1]', 'rounded-t-lg', 'grid');
+
+    const divCheckbox = document.createElement('div');
+    divCheckbox.classList.add('flex', 'justify-end', 'pr-3', 'pt-3');
+    const checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
+    checkbox.classList.add('form-checkbox', 'h-5', 'w-5');
+    divCheckbox.appendChild(checkbox);
+
+    const divContent = document.createElement('div');
+    divContent.classList.add('grid', 'justify-center');
+    const imgInteresse = document.createElement('img');
+    imgInteresse.src = `../img/${interesse.icone}`;
+    imgInteresse.setAttribute('alt', '');
+    imgInteresse.classList.add(interesse.classeIcone);
+    const h2Interesse = document.createElement('h2');
+    h2Interesse.textContent = interesse.nome;
+    divContent.appendChild(imgInteresse);
+    divContent.appendChild(h2Interesse);
+
+    divInteresse.appendChild(divCheckbox);
+    divInteresse.appendChild(divContent);
+
+    main.appendChild(divInteresse);
+});
+
+const divFinal = document.createElement('div');
+divFinal.classList.add('flex', 'items-center', 'gap-2');
+const linkContinuar = document.createElement('a');
+linkContinuar.setAttribute('href', '');
+linkContinuar.textContent = 'CONTINUAR';
+const imgSeta = document.createElement('img');
+imgSeta.setAttribute('src', '../img/gravity-ui_arrow-up.png');
+imgSeta.setAttribute('alt', '');
+divFinal.appendChild(linkContinuar);
+divFinal.appendChild(imgSeta);
+main.appendChild(divFinal);
+
+
+}
+carregarCategorias()

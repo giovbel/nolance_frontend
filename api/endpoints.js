cc9733730d0cbfe253d38f8d1503ef675bf06fa2
Estela Alves <123739850+estelalm@users.noreply.github.com>
2024-06-05 23:22:49 -0300

    endpoints usuario

--- api/endpoints.js
+++ api/endpoints.js
@@ -5,7 +5,7 @@ const baseUrl = 'http://localhost:8080/v1/nolance'
 
 export const getUsuarioById = async (id) =>{
 
-    const url = `${baseUrl}/usuario/${id}`
+    const url = `${baseUrl}/user/${id}`
     const response = await fetch(url)
     const data = await response.json()
 
@@ -15,10 +15,26 @@ export const getUsuarioById = async (id) =>{
 export const getLogarUsuario = async (email, senha) =>{
 
     
-    const url = `${baseUrl}/usuario?email=${email}&senha=${senha}`
+    const url = `${baseUrl}/user?email=${email}&senha=${senha}`
     console.log(url)
     const response = await fetch(url)
     const data = await response.json()
 
     return data.usuario
+}
+
+export async function postUsuario (usuario) {
+    const url = `${baseUrl}/users`
+    const options = {
+        method: "POST",
+        headers: {
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify(usuario)
+    }
+
+    const response = await fetch(url, options)
+
+    console.log(response.json())
+    return response.ok
 }
